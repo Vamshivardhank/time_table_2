dc5ad3e35f65fe9f28410b82c1daa784a69cd8a6
vamshivardhank <vamshivardhank@gmail.com>
2023-06-15 16:00:46 +0530

    y

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path=require('path');
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
-app.get('/',(req,res)=>{
+app.get('/home',(req,res)=>{
     res.render('index.ejs');
 });
 
